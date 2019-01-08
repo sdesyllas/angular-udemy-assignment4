735478a8ca23c2a0d0c4377d4e6832bee61153df
Spyros Desyllas <spyros2283@gmail.com>
2019-01-08 14:42:41 +0000

    CounterService implemented and injected in UserService

--- services-assignment/src/app/app.module.ts
+++ services-assignment/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { ActiveUsersComponent } from './active-users/active-users.component';
 import { InactiveUsersComponent } from './inactive-users/inactive-users.component';
 import { UserService } from './services/user.service';
+import { CounterService } from './services/counter.service';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,7 @@ import { UserService } from './services/user.service';
     FormsModule,
     HttpClientModule
   ],
-  providers: [UserService],
+  providers: [UserService, CounterService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- services-assignment/src/app/services/user.service.ts
+++ services-assignment/src/app/services/user.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import { CounterService } from '../services/counter.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private counterService: CounterService) { }
 
   activeUsers = ['Max', 'Anna'];
   inactiveUsers = ['Chris', 'Manu'];
@@ -13,10 +14,12 @@ export class UserService {
   setToInactive(id: number) {
     this.inactiveUsers.push(this.activeUsers[id]);
     this.activeUsers.splice(id, 1);
+    this.counterService.incrementActiveToInactive();
   }
 
   setToActive(id: number) {
     this.activeUsers.push(this.inactiveUsers[id]);
     this.inactiveUsers.splice(id, 1);
+    this.counterService.incrementInactiveToActive();
   }
 }
